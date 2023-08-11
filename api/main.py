c3367aeee6cd827b2ac3691f601b253ee7d0f5ac
oscardibarrag <oscar.ibarra@train.ia.center>
2023-08-11 15:38:21 -0600

    api_2

--- api/main.py
+++ api/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Libro(BaseModel):
+    titulo: str
+    autor: str
+    paginas: int
+    editorial: Optional[str]
 
 #Definir una ruta
 @app.get("/")
@@ -14,3 +22,10 @@ def read_item(item_id: int, m: str = None):
      return {"item_id": item_id, "m":m}
 #127.0.0.1:8000/items/808/hola_mundo
 
+@app.get("/Libors/{id}")
+async def mostrar_libro(id: int):
+    return {"data":id}
+
+@app.post("/Libros/")
+async def insertar_libro(libro: Libro):
+    return {"message": f"Libro: {libro.titulo} insertado correctamente"} 
